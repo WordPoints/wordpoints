3929c103a16ddf235c9ce17f7687b57564e4bd01
JDGrimes <jdg@codesymphony.co>
2014-01-14 12:00:56 -0500

    Don't award or remove points for non-public points types

--- src/components/points/includes/hooks.php
+++ src/components/points/includes/hooks.php
@@ -200,8 +200,12 @@ class WordPoints_Post_Points_Hook extends WordPoints_Points_Hook {
 
 			if (
 				(
-					$instance['post_type'] == 'ALL'
-					|| $instance['post_type'] == $post->post_type
+					$instance['post_type'] == $post->post_type
+					|| (
+						$instance['post_type'] == 'ALL'
+						&& post_type_exists( $post->post_type )
+						&& get_post_type_object( $post->post_type )->public
+					)
 				)
 				&& ! $this->awarded_points_already( $post->ID, $points_type )
 			) {
@@ -216,6 +220,7 @@ class WordPoints_Post_Points_Hook extends WordPoints_Points_Hook {
 	 *
 	 * @since 1.0.0
 	 * @since 1.1.0 The post_type is now passed as metadata when points are awarded.
+	 * @since 1.1.2 Points are only removed if the post type is public.
 	 *
 	 * @action before_delete_post Added by the constructor.
 	 *
@@ -232,8 +237,12 @@ class WordPoints_Post_Points_Hook extends WordPoints_Points_Hook {
 			if (
 				! empty( $instance['post_type'] )
 				&& (
-					$instance['post_type'] == 'ALL'
-					|| $instance['post_type'] == $post->post_type
+					$instance['post_type'] == $post->post_type
+					|| (
+						$instance['post_type'] == 'ALL'
+						&& post_type_exists( $post->post_type )
+						&& get_post_type_object( $post->post_type )->public
+					)
 				)
 				&& $post->post_status !== 'auto-draft'
 				&& $post->post_title !== __( 'Auto Draft', 'default' )

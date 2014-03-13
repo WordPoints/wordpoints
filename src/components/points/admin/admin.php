cc4e4259c380a8649d0a9c9b348a18e002caec03
JDGrimes <jdg@codesymphony.co>
2014-03-13 17:35:18 -0400

    Unslash the points change reason from the profile page before saving it
    Otherwise we’ll be double-shashing data.

--- src/components/points/admin/admin.php
+++ src/components/points/admin/admin.php
@@ -291,7 +291,7 @@ function wordpoints_points_profile_options_update( $user_id ) {
 
 		if ( isset( $_POST[ "wordpoints_points_set-{$slug}" ], $_POST[ "wordpoints_points-{$slug}" ], $_POST[ "wordpoints_points_old-{$slug}" ] ) ) {
 
-			wordpoints_alter_points( $user_id, $_POST[ "wordpoints_points-{$slug}" ] - $_POST[ "wordpoints_points_old-{$slug}" ], $slug, 'profile_edit', array( 'user_id' => get_current_user_id(), 'reason' => $_POST['wordpoints_set_reason'] ) );
+			wordpoints_alter_points( $user_id, $_POST[ "wordpoints_points-{$slug}" ] - $_POST[ "wordpoints_points_old-{$slug}" ], $slug, 'profile_edit', array( 'user_id' => get_current_user_id(), 'reason' => wp_unslash( $_POST['wordpoints_set_reason'] ) ) );
 		}
 	}
 }

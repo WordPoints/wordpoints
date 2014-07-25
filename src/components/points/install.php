98595862ae1bc49d8577e3f9fb0f95bc3c7e10d4
JDGrimes <jdg@codesymphony.co>
2014-07-25 17:34:09 -0400

    Set the charset when creating the database
    Includes the following database schema changes:
    
    - We now set the charset and collation, like WordPress core does.
    - We don’t try to specify a name for `PRIMARY` keys, since they can’t
    be named and `dbDelta()` chokes on them.
    - The `points_type` and `log_type` fields of the points logs table are
    `VARCHAR(255)` instead of `256`, for compatibility with MySQL 5.0.3,
    and because `dbDelta()` tries to create duplicate keys otherwise (the
    key names are returned from the database with the length, 255, appended
    to them, and don’t match the names in the SQL schema).
    
    The SQL schema has been moved into a function, and is now fully
    parseable by `dbDelta()`, which means that it can be used to update the
    database, not just create it.
    
    See #97

--- src/components/points/install.php
+++ src/components/points/install.php
@@ -15,8 +15,6 @@ if ( ! isset( $wordpoints_data ) ) {
 	exit;
 }
 
-global $wpdb;
-
 /**
  * Include the upgrade script so that we can use dbDelta() to create the DBs.
  *
@@ -24,50 +22,7 @@ global $wpdb;
  */
 require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 
-if ( ! wordpoints_db_table_exists( $wpdb->wordpoints_points_logs ) ) {
-
-	// - Create the table for the points transaction logs.
-
-	dbDelta(
-		'
-			CREATE TABLE ' . $wpdb->wordpoints_points_logs . '
-			(
-				id BIGINT(20) NOT NULL AUTO_INCREMENT,
-				user_id BIGINT(20) NOT NULL,
-				log_type VARCHAR(256) NOT NULL,
-				points BIGINT(20) NOT NULL,
-				points_type VARCHAR(256) NOT NULL,
-				text LONGTEXT,
-				blog_id SMALLINT(5) UNSIGNED NOT NULL,
-				site_id SMALLINT(5) UNSIGNED NOT NULL,
-				date DATETIME NOT NULL DEFAULT "0000-00-00 00:00:00",
-				PRIMARY KEY id (id),
-				KEY user_id (user_id),
-				KEY points_type (points_type),
-				KEY log_type (log_type)
-			);
-		'
-	);
-}
-
-if ( ! wordpoints_db_table_exists( $wpdb->wordpoints_points_log_meta ) ) {
-
-	// - Create the table to hold the metadata for points transations.
-
-	dbDelta(
-		'
-			CREATE TABLE ' . $wpdb->wordpoints_points_log_meta . ' (
-				meta_id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-				log_id BIGINT(20) UNSIGNED NOT NULL DEFAULT 0,
-				meta_key VARCHAR(255) DEFAULT NULL,
-				meta_value LONGTEXT,
-				PRIMARY KEY meta_id (meta_id),
-				KEY log_id (log_id),
-				KEY meta_key (meta_key)
-			);
-		'
-	);
-}
+dbDelta( wordpoints_points_get_db_schema() );
 
 // Add custom caps. Multisite is taken care of in the including function.
 if ( ! is_multisite() ) {

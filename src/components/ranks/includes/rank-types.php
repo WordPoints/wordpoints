aef95647866b5a951ce295e383ebc95d363dd0aa
JDGrimes <jdg@codesymphony.co>
2014-10-08 12:29:12 -0400

    Register rank group and type for each points type
    When the points component is active we register a rank group for each
    points type, and a points rank type for each.
    
    See #122

--- src/components/ranks/includes/rank-types.php
+++ src/components/ranks/includes/rank-types.php
@@ -14,4 +14,55 @@
  */
 include_once( WORDPOINTS_DIR . '/components/ranks/includes/rank-types/base.php' );
 
+if ( wordpoints_component_is_active( 'points' ) ) {
+	/**
+	 * The points rank type.
+	 *
+	 * @since 1.7.0
+	 */
+	include_once( WORDPOINTS_DIR . '/components/ranks/includes/rank-types/points.php' );
+}
+
+/**
+ * Register the included rank types.
+ *
+ * @since 1.7.0
+ *
+ * @WordPress\action wordpoints_ranks_register
+ */
+function wordpoints_register_core_ranks() {
+
+	if ( wordpoints_component_is_active( 'points' ) ) {
+
+		foreach ( wordpoints_get_points_types() as $slug => $points_type ) {
+
+			WordPoints_Rank_Groups::register_group(
+				"points_type-{$slug}"
+				, array(
+					'name' => $points_type['name'],
+					'description' => sprintf(
+						__(
+							'This rank type is associated with the "%s" points type.'
+							, 'wordpoints'
+						)
+						, $points_type['name']
+					)
+				)
+			);
+
+			WordPoints_Rank_Types::register_type(
+				"points-{$slug}"
+				, 'WordPoints_Points_Rank_Type'
+				, array( 'points_type' => $slug )
+			);
+
+			WordPoints_Rank_Groups::register_type_for_group(
+				"points-{$slug}",
+				"points_type-{$slug}"
+			);
+		}
+	}
+}
+add_action( 'wordpoints_ranks_register', 'wordpoints_register_core_ranks' );
+
 // EOF

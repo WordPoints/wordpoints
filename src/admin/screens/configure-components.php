56b387f34f1f7630fcdf2b0ef6b225d003682f59
JDGrimes <jdg@codesymphony.co>
2014-11-09 17:04:10 -0500

    Remove an unnecessary action attribute from a form
    We’re submitting the form to the same page, so it isn’t necessary.
    Also, the PHP generated value wasn’t being `echo`ed anyway.
    
    Fixes #204

--- src/admin/screens/configure-components.php
+++ src/admin/screens/configure-components.php
@@ -146,7 +146,7 @@ do_action( 'wordpoints_admin_components_top' );
 			<td><?php echo $component['description'] . $author; ?></td>
 			<td><?php echo $component['version']; ?></td>
 			<td>
-				<form method="post" name="wordpoints_components_form_<?php echo esc_attr( $component['slug'] ); ?>" action="<?php esc_attr( esc_url( self_admin_url( 'page=wordpoints_configure&tab=components' ) ) ); ?>">
+				<form method="post" name="wordpoints_components_form_<?php echo esc_attr( $component['slug'] ); ?>">
 					<input type="hidden" name="wordpoints_component_action" value="<?php echo esc_attr( $action ); ?>" />
 					<input type="hidden" name="wordpoints_component" value="<?php echo esc_attr( $component['slug'] ); ?>" />
 					<?php wp_nonce_field( "wordpoints_{$action}_component-{$component['slug']}" ); ?>

01c7a6c506e6b6326dd802c1fdbe45748ad6dd4d
JDGrimes <jdg@codesymphony.co>
2015-01-08 16:48:42 -0500

    Update git to SVN build to only copy the /src
    Fixes #245

--- tools/bin/git-to-svn.sh
+++ tools/bin/git-to-svn.sh
@@ -21,16 +21,11 @@ echo "Removing old files from SVN /$subpath..."
 sudo find "$svn/$subpath" -type f -exec rm '{}' ';'
 
 echo "Copying new files from git to SVN /$subpath..."
+cp -r "$git/src" "$svn/$subpath/"
 
-to_copy=( .jshintignore .jshintrc .travis.yml composer.json src composer.lock tests phpcs.ruleset.xml tools assets phpunit.xml.dist bin readme.txt wp-l10n-validator.json )
-
-for file in "${to_copy[@]}"; do
-	cp -r "$git/$file" "$svn/$subpath/"
-done
-
-echo 'Moving the assets directory...'
+echo 'Copying the assets directory...'
 sudo rm -rf "$svn/assets"
-mv "$svn/$subpath/assets" "$svn"
+cp -r "$git/assets" "$svn/assets"
 
 echo 'Done! You can now commit the changes.'
 

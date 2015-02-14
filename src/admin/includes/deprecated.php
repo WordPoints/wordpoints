9f2f5b9f097da78d06c1aff9e847d2e97da32f73
JDGrimes <jdg@codesymphony.co>
2015-02-14 16:10:08 -0500

    Remove deprecated core admin code
    See #281

--- src/admin/includes/deprecated.php
+++ src/admin/includes/deprecated.php
@@ -7,15 +7,4 @@
  * @since 1.7.0
  */
 
-/**
- * Display one of the administration screens.
- *
- * @since 1.1.0
- * @deprecated 1.7.0 No longer used.
- */
-function wordpoints_display_admin_screen() {
-
-	_deprecated_function( __FUNCTION__, '1.7.0' );
-}
-
 // EOF

0364da23ae28fdffadf52626b8eb85f3f49a8e3b
JDGrimes <jdg@codesymphony.co>
2015-04-19 16:33:02 -0400

    Remove duplicate code from 1.5.1 update test
    We’ve moved this code to helper methods where it can be reused.
    
    See #246

--- tests/phpunit/tests/points/update/1-5-1.php
+++ tests/phpunit/tests/points/update/1-5-1.php
@@ -24,6 +24,11 @@
 class WordPoints_Points_1_5_1_Update_Test extends WordPoints_Points_UnitTestCase {
 
 	/**
+	 * @since 2.0.0
+	 */
+	protected $previous_version = '1.5.0';
+
+	/**
 	 * Test that database table character sets are updated.
 	 *
 	 * @since 1.5.1
@@ -33,59 +38,12 @@ class WordPoints_Points_1_5_1_Update_Test extends WordPoints_Points_UnitTestCase
 	 */
 	public function test_db_table_charsets_updated() {
 
-		global $wpdb;
-
-		$wpdb->query( 'ROLLBACK' );
-
-		remove_filter( 'query', array( $this, '_create_temporary_tables' ) );
-		remove_filter( 'query', array( $this, '_drop_temporary_tables' ) );
-		remove_filter( 'query', array( $this, 'do_not_alter_tables' ) );
-
-		$wpdb->query( "DROP TABLE `{$wpdb->wordpoints_points_logs}`" );
-		$wpdb->query( "DROP TABLE `{$wpdb->wordpoints_points_log_meta}`" );
-
-		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-
-		// Create the tables without special charset.
-		dbDelta(
-			"CREATE TABLE {$wpdb->wordpoints_points_logs} (
-				id BIGINT(20) NOT NULL AUTO_INCREMENT,
-				user_id BIGINT(20) NOT NULL,
-				log_type VARCHAR(255) NOT NULL,
-				points BIGINT(20) NOT NULL,
-				points_type VARCHAR(255) NOT NULL,
-				text LONGTEXT,
-				blog_id SMALLINT(5) UNSIGNED NOT NULL,
-				site_id SMALLINT(5) UNSIGNED NOT NULL,
-				date DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
-				PRIMARY KEY  (id),
-				KEY user_id (user_id),
-				KEY points_type (points_type),
-				KEY log_type (log_type)
-			);
-			CREATE TABLE {$wpdb->wordpoints_points_log_meta} (
-				meta_id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-				log_id BIGINT(20) UNSIGNED NOT NULL DEFAULT 0,
-				meta_key VARCHAR(255) DEFAULT NULL,
-				meta_value LONGTEXT,
-				PRIMARY KEY  (meta_id),
-				KEY log_id (log_id),
-				KEY meta_key (meta_key)
-			);"
-		);
+		$this->create_tables_with_charset( 'latin1' );
 
 		// Simulate the update.
-		$this->update_component( 'points', '1.5.0' );
+		$this->update_component();
 
-		$this->assertStringMatchesFormat(
-			'%aDEFAULT CHARSET=utf8%A'
-			, $wpdb->get_var( "SHOW CREATE TABLE `{$wpdb->wordpoints_points_logs}`", 1 )
-		);
-
-		$this->assertStringMatchesFormat(
-			'%aDEFAULT CHARSET=utf8%A'
-			, $wpdb->get_var( "SHOW CREATE TABLE `{$wpdb->wordpoints_points_log_meta}`", 1 )
-		);
+		$this->assertTablesHaveCharset( 'utf8mb4' );
 
 		$this->start_transaction();
 		$this->create_points_type();

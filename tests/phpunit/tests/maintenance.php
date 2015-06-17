3d8065a7594f4604643e75c1b7743734db51bb83
JDGrimes <jdg@codesymphony.co>
2015-06-17 14:57:36 -0400

    Remove cruft from test case
    See #330

--- tests/phpunit/tests/maintenance.php
+++ tests/phpunit/tests/maintenance.php
@@ -80,38 +80,6 @@ class WordPoints_Maintenance_Test extends WordPoints_UnitTestCase {
 
 		$this->assertEmpty( $upgrading );
 	}
-	/**
-	 * Test is_wordpoints_network_active().
-	 *
-	 * @since 1.2.0
-	 *
-	 * @covers ::is_wordpoints_network_active
-	 */
-	public function is_wordpoints_network_active() {
-
-		/** @var int $upgrading */
-		global $upgrading;
-
-		$time = time();
-
-// Don't lock the user out of their site if the file fails to delete for some reason.
-		if ( $time - $upgrading >= 10 * MINUTE_IN_SECONDS ) {
-			return;
-		}
-
-// If we're not running a module check, let the maintenance message show.
-		if ( ! isset( $_GET['wordpoints_module_check'], $_GET['check_module'] ) ) {
-			return;
-		}
-
-// Normally we might try to verify the nonce here, however, the nonce functions are
-// pluggable and so won't be loaded until much later.
-
-// Trick wp_maintenance() into not showing the maintenance message.
-		$upgrading = $time - 10 * MINUTE_IN_SECONDS;
-
-	}
-
 }
 
 // EOF

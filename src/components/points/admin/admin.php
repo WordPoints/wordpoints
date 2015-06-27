05eb8d9e405902e85fd6c52db07b27f418c0ffa8
JDGrimes <jdg@codesymphony.co>
2015-06-27 18:14:50 -0400

    Add admin file back
    See #335

---
+++ src/components/points/admin/admin.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Points component administration.
+ *
+ * This code is run only on the administration pages. It registers the points
+ * administration panels, etc.
+ *
+ * @package WordPoints\Points\Administration
+ * @since 1.0.0
+ */
+
+/**
+ * AJAX callbacks.
+ *
+ * @since 1.2.0
+ */
+include_once WORDPOINTS_DIR . 'components/points/admin/includes/ajax.php';
+
+/**
+ * Admin-side functions.
+ *
+ * @since 2.1.0
+ */
+require_once( WORDPOINTS_DIR . '/components/points/admin/includes/functions.php' );
+
+/**
+ * Admin-side hooks.
+ *
+ * @since 2.1.0
+ */
+require_once( WORDPOINTS_DIR . '/components/points/admin/includes/filters.php' );
+
+// EOF

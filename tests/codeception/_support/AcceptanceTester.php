7f77c96df26799a1673dd8192eb6acddc20f741f
JDGrimes <jdg@codesymphony.co>
2016-06-22 10:23:46 -0400

    Hooks API merge: update dev-lib
    And create Codeception test scaffold.
    
    (Some parts of the Hooks API changes were merged into the dev lib to be
    available to all WordPoints projects.)
    
    See #321

---
+++ tests/codeception/_support/AcceptanceTester.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Acceptance tester class.
+ *
+ * @package wordpoints-dev-lib
+ * @since 2.1.0
+ */
+
+/**
+ * Tester for use in the acceptance tests.
+ *
+ * @since 2.1.0
+ */
+class AcceptanceTester extends \WordPoints\Tests\Codeception\AcceptanceTester {
+	// Add any methods here that you want.
+}
+
+// EOF

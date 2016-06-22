6b23d3977e659d93ec248c70c50622b7c4a047f3
JDGrimes <jdg@codesymphony.co>
2016-06-22 10:24:39 -0400

    Hooks API merge: Codeception tests
    See #321

---
+++ tests/codeception/acceptance/points/reaction/create.cept.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Tests creating a points reaction.
+ *
+ * @package WordPoints\Codeception
+ * @since 2.1.0
+ */
+
+$I = new AcceptanceTester( $scenario );
+$I->wantTo( 'Create a points reaction' );
+$I->hadCreatedAPointsType();
+$I->amLoggedInAsAdminOnPage( 'wp-admin/admin.php?page=wordpoints_points_types' );
+$I->click( 'Add New', '#points-user_register' );
+$I->waitForNewReaction();
+$I->seeElement( '#points-user_register .add-reaction[disabled]' );
+$I->fillField( 'description', 'Registering.' );
+$I->fillField( 'log_text', 'Registration.' );
+$I->fillField( 'points', '10' );
+$I->click( 'Save', '#points-user_register' );
+$I->waitForJqueryAjax();
+$I->see( 'Your changes have been saved.', '#points-user_register .messages' );
+$I->canSeeElement( '#points-user_register .add-reaction:not([disabled])' );
+
+// EOF
---
+++ tests/codeception/acceptance/points/reaction/delete.cept.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Tests deleting a points reaction.
+ *
+ * @package WordPoints\Codeception
+ * @since 2.1.0
+ */
+
+$I = new AcceptanceTester( $scenario );
+$I->wantTo( 'Delete a points reaction' );
+$I->hadCreatedAPointsReaction();
+$I->amLoggedInAsAdminOnPage( 'wp-admin/admin.php?page=wordpoints_points_types' );
+$I->see( 'Test description.', '#points-user_register .wordpoints-hook-reaction .title' );
+$I->click( 'Edit', '#points-user_register .wordpoints-hook-reaction' );
+$I->canSeeInFormFields(
+	'#points-user_register .wordpoints-hook-reaction form'
+	, array(
+		'description' => 'Test description.',
+		'log_text' => 'Test log text.',
+		'points' => '10',
+	)
+);
+$I->click( 'Delete', '#points-user_register .wordpoints-hook-reaction' );
+$I->seeJQueryDialog( 'Are you sure?' );
+$I->click( 'Delete', '.wordpoints-delete-hook-reaction-dialog' );
+$I->waitForJqueryAjax();
+$I->cantSeeElement( '#points-user_register .wordpoints-hook-reaction' );
+
+// EOF

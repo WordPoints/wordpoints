e5e31dbe1121f8a842306df17436093a5dfa9777
JDGrimes <jdg@codesymphony.co>
2016-10-06 17:56:07 -0400

    Introduce entity context switching
    The site context may be switched on multisite, and network switching is provided if a plugin, like WP Multi Network, is installed that provides network switching functions. This should also work if WordPress core implements network switching in the future.
    
    Fixes #479

--- src/classes/entity/context/network.php
+++ src/classes/entity/context/network.php
@@ -27,6 +27,30 @@ class WordPoints_Entity_Context_Network extends WordPoints_Entity_Context {
 
 		return (int) get_current_site()->id;
 	}
+
+	/**
+	 * @since 2.2.0
+	 */
+	public function switch_to( $id ) {
+
+		if ( function_exists( 'switch_to_network' ) ) {
+			return switch_to_network( $id );
+		}
+
+		return false;
+	}
+
+	/**
+	 * @since 2.2.0
+	 */
+	public function switch_back() {
+
+		if ( function_exists( 'restore_current_network' ) ) {
+			return restore_current_network();
+		}
+
+		return false;
+	}
 }
 
 // EOF

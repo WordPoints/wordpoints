cef4c501a32d56d550f7de258883e920a5e1f1a7
JDGrimes <jdg@codesymphony.co>
2016-11-05 09:33:16 -0400

    Don't refire hooks on post update
    The post publish and post depublish actions were both firing when a post was updated, because the `transition_post_status` action fires even when both the old and new statuses are the same. Since these two hooks were just listening for the new and old statuses to be `publish`, respectively, when updating a post and both the old and new statuses were `publish`, both of these actions would fire, resulting in points being awarded and removed at once.
    
    Note that our tests for these hooks were actually incorrect, as they were expecting the log to be reversed by setting the post status to `publish` instead of `draft`—meaning that our tests were actually relying on this behavior!
    
    Fixes #550

--- src/includes/hooks.php
+++ src/includes/hooks.php
@@ -211,7 +211,10 @@ function wordpoints_register_post_type_hook_actions( $slug ) {
 				'action' => 'transition_post_status',
 				'data'   => array(
 					'arg_index'    => array( "post\\{$slug}" => 2 ),
-					'requirements' => array( 0 => 'publish' ),
+					'requirements' => array(
+						0 => 'publish',
+						1 => array( 'comparator' => '!=', 'value' => 'publish' ),
+					),
 				),
 			)
 		);
@@ -223,7 +226,10 @@ function wordpoints_register_post_type_hook_actions( $slug ) {
 				'action' => 'transition_post_status',
 				'data'   => array(
 					'arg_index'    => array( "post\\{$slug}" => 2 ),
-					'requirements' => array( 1 => 'publish' ),
+					'requirements' => array(
+						0 => array( 'comparator' => '!=', 'value' => 'publish' ),
+						1 => 'publish',
+					),
 				),
 			)
 		);

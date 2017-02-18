6d62fda3cb0626071dcc3dec2663102e973b6983
JDGrimes <jdg@codesymphony.co>
2017-02-18 10:01:40 -0500

    Fix legacy Post Publish event tests
    The tests for the event for hierarchical post types were failing, because we were not creating a parent post. We had already fixed this for the tests for the regular post publish event, but not the legacy version of the event registered for the points component for imported hooks.
    
    See #422

--- tests/phpunit/tests/points/classes/hook/event/post/publish/legacy/page.php
+++ tests/phpunit/tests/points/classes/hook/event/post/publish/legacy/page.php
@@ -21,6 +21,24 @@ class WordPoints_Points_Hook_Event_Post_Publish_Legacy_Page_Test
 	 * @since 2.1.0
 	 */
 	protected $dynamic_slug = 'page';
+
+	/**
+	 * @since 2.3.0
+	 */
+	protected $expected_targets = array(
+		array( 'post\\', 'author', 'user' ),
+		array( 'post\\', 'parent', 'post\\', 'author', 'user' ),
+	);
+
+	/**
+	 * @since 2.3.0
+	 */
+	protected function create_post( array $args = array() ) {
+
+		$args['post_parent'] = parent::create_post();
+
+		return parent::create_post( $args );
+	}
 }
 
 // EOF

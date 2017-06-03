1931c3204594f1851b268b9cbe4c8b0be17934a6
JDGrimes <jdg@codesymphony.co>
2017-06-03 17:06:50 -0400

    Rename network extension acceptance tests
    The regular tests were already renamed, but we missed the network one.
    
    See #668

--- tests/codeception/acceptance/network/extension/activate.cept.php
+++ tests/codeception/acceptance/network/extension/activate.cept.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Tests network-activating a module.
+ * Tests network-activating an extension.
  *
  * @package WordPoints\Codeception
  * @since 2.2.0
@@ -12,16 +12,16 @@ if ( ! is_wordpoints_network_active() ) {
 }
 
 $I = new AcceptanceTester( $scenario );
-$I->wantTo( 'Network-activate a module' );
+$I->wantTo( 'Network-activate an extension' );
 $I->haveTestModuleInstalled( 'module-7' );
 $I->haveTestModuleInstalled( 'test-6' );
 $I->hadActivatedModule( 'test-6/main-file.php', true );
 $I->amLoggedInAsAdminOnPage( 'wp-admin/network/admin.php?page=wordpoints_modules' );
-$I->see( 'WordPoints Modules', '.wrap h1' );
+$I->see( 'WordPoints Extensions', '.wrap h1' );
 $I->see( 'Network Deactivate', 'tr#test-6' );
 $I->click( 'Network Activate', 'tr#module-7' );
-$I->see( 'WordPoints Modules', '.wrap h1' );
-$I->see( 'Module activated.', '.notice-success' );
+$I->see( 'WordPoints Extensions', '.wrap h1' );
+$I->see( 'Extension activated.', '.notice-success' );
 $I->see( 'Network Deactivate', 'tr#module-7' );
 $I->see( 'Network Deactivate', 'tr#test-6' );
 

fce640e83b2d5ffd037d17c58bcf3841ebdcb847
JDGrimes <jdg@codesymphony.co>
2017-09-07 10:52:35 -0400

    Entities API: don't rely on current context in attribute class
    In #535 it became possible to retrieve entities from a different context
    than the entity actually exists in, by passing in the full GUID of the
    entity. Switching to the proper context to retrieve the entity was now
    handled internally.
    
    However, when retrieving entity children, like an entity attribute, the
    current context was still always assumed to be correct. This commit
    fixes that in regard to attributes, by ensuring that the correct context
    is switched to when retrieving the attribute value.
    
    The base implementation here only covers the most common form of
    attribute, where the attribute value comes from the same context as the
    entity. For attributes where the value comes from a different context,
    special handling would be needed to fully support out-of-context
    retrieval of the attribute value. Though this situation is unlikely to
    exist in practice.
    
    See #536

--- tests/phpunit/tests/classes/entity/attr.php
+++ tests/phpunit/tests/classes/entity/attr.php
@@ -55,7 +55,7 @@ class WordPoints_Entity_Attr_Test extends WordPoints_PHPUnit_TestCase_Hooks {
 		$entity = new WordPoints_PHPUnit_Mock_Entity( 'test' );
 
 		$attr = new WordPoints_PHPUnit_Mock_Entity_Attr( 'test' );
-		$this->assertTrue( $attr->set_the_value_from_entity( $entity ) );
+		$this->assertFalse( $attr->set_the_value_from_entity( $entity ) );
 
 		$this->assertNull( $attr->get_the_value() );
 	}
@@ -91,13 +91,175 @@ class WordPoints_Entity_Attr_Test extends WordPoints_PHPUnit_TestCase_Hooks {
 
 		$this->assertSame( 'a', $attr->get_the_value() );
 
-		$this->assertTrue(
+		$this->assertFalse(
 			$attr->set_the_value_from_entity(
 				new WordPoints_PHPUnit_Mock_Entity( 'test' )
 			)
 		);
 
 		$this->assertNull( $attr->get_the_value() );
+	}
+
+	/**
+	 * Test setting the value from a different context.
+	 *
+	 * @since 2.4.0
+	 */
+	public function test_set_the_value_from_entity_different_context() {
+
+		$context = $this->factory->wordpoints->entity_context->create_and_get(
+			array( 'slug' => 'test_context' )
+		);
+
+		$entity = $this->getPartialMockForAbstractClass(
+			'WordPoints_Entity'
+			, array( 'get_the_attr_value', 'get_the_context' )
+			, array( 'test' )
+		);
+
+		$entity->method( 'get_the_context' )
+			->willReturn( array( $context->get_slug() => 2 ) );
+
+		$entity->method( 'get_the_attr_value' )
+			->willReturnCallback( array( $context, 'get_current_id' ) );
+
+		$attr = new WordPoints_PHPUnit_Mock_Entity_Attr( 'test' );
+
+		$this->assertSame( 1, $context->get_current_id() );
+
+		$this->assertTrue( $attr->set_the_value_from_entity( $entity ) );
+		$this->assertSame( 2, $attr->get_the_value() );
+
+		$this->assertSame( 1, $context->get_current_id() );
+	}
+
+	/**
+	 * Test setting the value from the same context.
+	 *
+	 * @since 2.4.0
+	 */
+	public function test_set_the_value_from_entity_same_context() {
+
+		$context = $this->factory->wordpoints->entity_context->create_and_get(
+			array( 'slug' => 'test_context' )
+		);
+
+		$entity = $this->getPartialMockForAbstractClass(
+			'WordPoints_Entity'
+			, array( 'get_the_attr_value', 'get_the_context' )
+			, array( 'test' )
+		);
+
+		$entity->method( 'get_the_context' )
+			->willReturn( array( $context->get_slug() => 1 ) );
+
+		$entity->method( 'get_the_attr_value' )
+			->willReturnCallback( array( $context, 'get_current_id' ) );
+
+		$attr = new WordPoints_PHPUnit_Mock_Entity_Attr( 'test' );
+
+		$this->assertSame( 1, $context->get_current_id() );
+
+		$this->assertTrue( $attr->set_the_value_from_entity( $entity ) );
+		$this->assertSame( 1, $attr->get_the_value() );
+
+		$this->assertSame( 1, $context->get_current_id() );
+	}
+
+	/**
+	 * Test setting the value from an entity with no context set.
+	 *
+	 * @since 2.4.0
+	 */
+	public function test_set_the_value_from_entity_context_not_set() {
+
+		$context = $this->factory->wordpoints->entity_context->create_and_get(
+			array( 'slug' => 'test_context' )
+		);
+
+		$entity = $this->getPartialMockForAbstractClass(
+			'WordPoints_Entity'
+			, array( 'get_the_attr_value' )
+			, array( 'test' )
+		);
+
+		$entity->method( 'get_the_attr_value' )
+			->willReturnCallback( array( $context, 'get_current_id' ) );
+
+		$attr = new WordPoints_PHPUnit_Mock_Entity_Attr( 'test' );
+
+		$this->assertSame( 1, $context->get_current_id() );
+
+		$this->assertFalse( $attr->set_the_value_from_entity( $entity ) );
+		$this->assertNull( $attr->get_the_value() );
+
+		$this->assertSame( 1, $context->get_current_id() );
+	}
+
+	/**
+	 * Test setting the value from an entity with a nonexistent context.
+	 *
+	 * @since 2.4.0
+	 */
+	public function test_set_the_value_from_entity_context_nonexistent() {
+
+		$context = $this->factory->wordpoints->entity_context->create_and_get(
+			array( 'slug' => 'test_context' )
+		);
+
+		$entity = $this->getPartialMockForAbstractClass(
+			'WordPoints_Entity'
+			, array( 'get_the_attr_value', 'get_the_context' )
+			, array( 'test' )
+		);
+
+		$entity->method( 'get_the_context' )
+			->willReturn( array( 'nonexistent_context' => 5 ) );
+
+		$entity->method( 'get_the_attr_value' )
+			->willReturnCallback( array( $context, 'get_current_id' ) );
+
+		$attr = new WordPoints_PHPUnit_Mock_Entity_Attr( 'test' );
+
+		$this->assertSame( 1, $context->get_current_id() );
+
+		$this->assertFalse( $attr->set_the_value_from_entity( $entity ) );
+		$this->assertNull( $attr->get_the_value() );
+
+		$this->assertSame( 1, $context->get_current_id() );
+	}
+
+	/**
+	 * Test setting the value from an entity from the global context.
+	 *
+	 * @since 2.4.0
+	 */
+	public function test_set_the_value_from_entity_global_context() {
+
+		$context = $this->factory->wordpoints->entity_context->create_and_get(
+			array( 'slug' => 'test_context' )
+		);
+
+		$entity = $this->getPartialMockForAbstractClass(
+			'WordPoints_Entity'
+			, array( 'get_the_attr_value', 'get_the_context' )
+			, array( 'test' )
+		);
+
+		$entity->method( 'get_the_context' )
+			->willReturn( array() );
+
+		$entity->method( 'get_the_attr_value' )
+			->willReturnCallback( array( $context, 'get_current_id' ) );
+
+		$attr = new WordPoints_PHPUnit_Mock_Entity_Attr( 'test' );
+
+		$this->assertSame( 1, $context->get_current_id() );
+
+		$this->assertTrue( $attr->set_the_value_from_entity( $entity ) );
+		$this->assertSame( 1, $attr->get_the_value() );
+
+		$this->assertSame( 1, $context->get_current_id() );
 	}
 }
 

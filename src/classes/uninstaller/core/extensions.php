cce57c2789918bd3fe29bc49dfdab9ee8b0acdbb
JDGrimes <jdg@codesymphony.co>
2017-09-11 10:38:38 -0400

    Uninstall: use custom filter for legacy extensions path
    Our regular filter is optimized not to check the filesystem, but as a
    result it ends up calling `is_wordpoints_network_active()`, which is
    unreliable during uninstall.
    
    See #399

--- src/classes/uninstaller/core/extensions.php
+++ src/classes/uninstaller/core/extensions.php
@@ -19,6 +19,14 @@ class WordPoints_Uninstaller_Core_Extensions implements WordPoints_RoutineI {
 	 */
 	public function run() {
 
+		// We use a custom filter on the legacy path, because the default one relies
+		// on WordPoints's activation status.
+		remove_filter( 'wordpoints_extensions_dir', 'wordpoints_legacy_modules_path', 5 );
+		remove_filter( 'wordpoints_extensions_url', 'wordpoints_legacy_modules_path', 5 );
+
+		add_filter( 'wordpoints_extensions_dir', array( $this, 'legacy_path' ), 5 );
+		add_filter( 'wordpoints_extensions_url', array( $this, 'legacy_path' ), 5 );
+
 		require_once WORDPOINTS_DIR . '/includes/modules.php';
 
 		add_filter( 'is_wordpoints_extension_active', '__return_false' );
@@ -32,6 +40,30 @@ class WordPoints_Uninstaller_Core_Extensions implements WordPoints_RoutineI {
 		if ( $wp_filesystem instanceof WP_Filesystem_Base ) {
 			$wp_filesystem->delete( wordpoints_extensions_dir(), true );
 		}
+	}
+
+	/**
+	 * Filters the extension path to correct it to the legacy path if needed.
+	 *
+	 * @since 2.4.0
+	 *
+	 * @param string $path The extensions path.
+	 *
+	 * @return string The filtered path.
+	 */
+	public function legacy_path( $path ) {
+
+		$new_path = WP_CONTENT_DIR . '/wordpoints-extensions';
+
+		if ( false !== strpos( $path, $new_path ) && ! is_dir( $new_path ) ) {
+			$path = str_replace(
+				'/wordpoints-extensions'
+				, '/wordpoints-modules'
+				, $path
+			);
+		}
+
+		return $path;
 	}
 }
 
